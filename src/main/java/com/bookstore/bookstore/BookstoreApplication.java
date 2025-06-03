fbd6acf1840838ee740bc74db002dce67a1483f9
toghrulmammdl <togrul9871@gmail.com>
2025-06-03 15:12:42 +0400

    Update BookstoreApplication.java

--- src/main/java/com/bookstore/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/bookstore/BookstoreApplication.java
@@ -1,13 +1,24 @@
 package com.bookstore.bookstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
+		try{
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry->System.setProperty(entry.getKey(), entry.getValue()));
+
+		} catch (io.github.cdimascio.dotenv.DotenvException e){
+			System.err.println("Warning: .env file not found or could not be loaded. Ensure environment variables are set externally if this is not a development environment.");
+		}
+
 		SpringApplication.run(BookstoreApplication.class, args);
+
 	}
 
 }
